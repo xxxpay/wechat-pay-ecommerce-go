1245c1e4f1934f139d7a0144833b31a241347bc2
newtalentxp <52388483@qq.com>
2020-07-15 07:25:00 +0800

    fix

---
+++ core/v3/certificate.go
@@ -0,0 +1,32 @@
+package v3
+
+import (
+	"encoding/json"
+	"net/http"
+	"wechat-pay-ecommerce-sdk/core"
+	"wechat-pay-ecommerce-sdk/dto"
+)
+
+func (c *payClient) Certificate() (*dto.CertificateResp, error) {
+	body, err := c.doRequest(nil, core.BuildUrl(nil, nil, core.ApiCertification), http.MethodGet)
+	if err != nil {
+		return nil, err
+	}
+	var resp dto.CertificateResp
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+	for _, data := range resp.Data {
+		encryptCert := data.EncryptCertificate
+		if encryptCert == nil {
+			continue
+		}
+		decryptCert, err := c.Decrypt(encryptCert.Algorithm, encryptCert.Ciphertext, encryptCert.AssociatedData, encryptCert.Nonce)
+		if err != nil {
+			return nil, err
+		}
+		data.DecryptCertificate = string(decryptCert)
+	}
+	return &resp, nil
+}
